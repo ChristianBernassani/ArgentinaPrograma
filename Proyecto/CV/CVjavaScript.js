3816c20b05df36f09d0137378250028e31aeaf7d
ChristianBernassani <christianbernassani@gmail.com>
2023-01-20 03:57:55 -0300

    Primera subida de archivos
    Se subieron los archivos html, css, javascript y las imagenes utilizadas para el CV

---
+++ Proyecto/CV/CVjavaScript.js
@@ -0,0 +1,12 @@
+function mensajeInicial() {
+    alert("Si estas viendo esto es porque la pagina cargo correctamente. Espero disfrute este cv. PD: Presione la imagen de perfil");
+}
+
+function cambiarImagen(obj) {
+    document.getElementById('fotoDePerfil').src = "C:/Users/Chris/Desktop/Argentina%20programa/CV/Imagenes/PhoenixCircle.png";
+}
+
+function imagenOriginal(obj) {
+    document.getElementById('fotoDePerfil').src = "C:/Users/Chris/Desktop/Argentina%20programa/CV/Imagenes/GladiadorCircle.png";
+}
+
